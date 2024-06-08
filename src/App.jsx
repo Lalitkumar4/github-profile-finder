4bbdfd09f5533dda63d4859dc6b79f3cf4b4f089
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2024-06-08 11:59:35 +0530

    footer complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import Footer from "./components/layout/Footer"
 import Navbar from "./components/layout/Navbar"
 
 function App() {
   return (
-    <div>
+    <div className="bg-[#0D1117]">
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-      </div>
 
-      <main>Content</main>
+        <main>Content</main>
+
+        <Footer />
+      </div>
     </div>
   )
 }
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p className="mb-4 text-lg text-center text-gray-400">
+        Build and designed by{" "}
+        <a
+          className="underline hover:text-white"
+          href="https://github.com/Lalitkumar4"
+          target="_blank"
+        >
+          Lalit Kumar
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
