7b122ea655c98210264c8ad8aa520abc257e365c
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2024-06-08 12:02:13 +0530

    remove some classes

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 
 const Navbar = ({ title }) => {
   return (
-    <nav className="p-4 mb-12 shadow-lg dark:bg-[#161B22] dark:text-white bg-white text-black">
+    <nav className="p-4 mb-12 shadow-lg bg-[#161B22] text-white">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
           <FaGithub className="inline pr-2 text-3xl" />
