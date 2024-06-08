f2fda2387c7fc86681386ecd83df11dcc289207c
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2024-06-08 14:10:24 +0530

    setup routes and notFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 import Footer from "./components/layout/Footer"
 import Navbar from "./components/layout/Navbar"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
-    <div className="bg-[#0D1117]">
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
+    <BrowserRouter>
+      <div className="bg-[#0D1117]">
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
 
-        <main>Content</main>
+          <main className="container px-3 pb-12 mx-auto">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
 
-        <Footer />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,27 @@
+import { FaArrowLeft } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+  return (
+    <div className="hero">
+      <div className="text-center hero-content">
+        <h1 className="mb-8 font-bold text-blue-700 text-8xl">404</h1>
+        <p className="mb-3 text-2xl text-white">
+          Looks like this page doesn&apos;t exist!
+        </p>
+        <p className="mb-10 text-gray-300">
+          Go back to home and continue exploring.
+        </p>
+        <Link
+          to="/"
+          className="p-4 text-white bg-blue-700 rounded-full hover:p-5 hover:bg-blue-500 hover:transition-all"
+        >
+          <FaArrowLeft className="inline mr-2" />
+          Go Home
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
