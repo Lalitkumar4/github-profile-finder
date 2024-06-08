435dfc69093ddc3b4a44e936636498e9feaf98a9
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2024-06-08 12:07:03 +0530

    change logo size

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ title }) => {
     <nav className="p-4 mb-12 shadow-lg bg-[#161B22] text-white">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-          <FaGithub className="inline pr-2 text-3xl" />
+          <FaGithub className="inline pr-2 text-4xl" />
           <span className="text-lg font-bold align-middle">{title}</span>
         </div>
       </div>
