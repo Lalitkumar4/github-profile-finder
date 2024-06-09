bd9b6b06c3ab9e90dceb91b074c889981ab37eb0
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2024-06-09 14:48:27 +0530

    add context api to manage states

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,29 @@ import Home from "./pages/Home"
 import Navbar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
 import NotFound from "./pages/NotFound"
+import { GithubProvider } from "./context/GithubContext"
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+    <GithubProvider>
+      <BrowserRouter>
+        <div>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-          <main className="container px-3 pb-12 mx-auto">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </main>
+            <main className="container px-3 pb-12 mx-auto">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
 
-          <Footer />
+            <Footer />
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </GithubProvider>
   )
 }
 
---
+++ src/context/GithubContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useReducer } from "react"
+import githubReducer from "./GithubReducer"
+
+const GithubContext = createContext()
+
+// eslint-disable-next-line react/prop-types
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: true,
+  }
+
+  const [state, dispatch] = useReducer(githubReducer, initialState)
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`)
+
+    const data = await response.json()
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    })
+  }
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export default GithubContext
