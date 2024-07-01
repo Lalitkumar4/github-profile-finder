dd6268f9c071ed7f234792d111582f1f4d0a5bbd
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2024-07-01 18:05:02 +0530

    add a click event to the navbar logo to navigate to the home

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,17 @@
 import PropTypes from "prop-types"
+import { useNavigate } from "react-router-dom"
 import { FaGithub } from "react-icons/fa"
 
 const Navbar = ({ title = "Github User Explorer" }) => {
+  const navigate = useNavigate()
+
   return (
     <nav className="p-4 mb-8 shadow-lg bg-[#161B22] text-white">
       <div className="container mx-auto">
-        <div className="flex-none px-2 mx-2">
+        <div
+          onClick={() => navigate("/")}
+          className="flex-none inline px-2 mx-2 cursor-pointer"
+        >
           <FaGithub className="inline pr-2 text-4xl" />
           <span className="text-lg font-bold align-middle">{title}</span>
         </div>
